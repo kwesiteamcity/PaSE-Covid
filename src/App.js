e4159d27eeee79d502f6b6fed1a53d989839a832
kwesiteamcity <68638570+kwesiteamcity@users.noreply.github.com>
2020-08-15 13:50:18 +1200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import Logo from './components/Logo'
+import Cards from './components/Cards'
+import Rowtwo from './components/Rowtwo'
+
+
+class App extends  Component{
+  render(){
+    return (
+      <div>
+        <Logo/>
+        <Cards/>
+        <Rowtwo/>
+        
+        
+        
+        
+      </div>
+    )
+  }
+}
+
+export default App;
